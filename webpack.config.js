8e7fdee8a446ad2858a30391a6d1f7e6161bdef3
Mendel Konikov <mkonikov@gmail.com>
2017-09-06 15:41:46 -0400

    Build skeleton

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './lib/entry.js',
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps'
+};
